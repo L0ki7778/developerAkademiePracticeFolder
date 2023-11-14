ea7d1896274dc29da3c3adbaad9ef72d40e1b03e
Rene Heller <terra7778@gmail.com>
2023-11-14 12:27:48 +0100

    quiz-card

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function render(){
-   let content = document.getElementById("contacts");
-    content.innerHTML = ``
-    content.innerHTML =`<h1>My Contacts</h1>`
-}
+// function render(){
+//    let content = document.getElementById("contacts");
+//     content.innerHTML = ``
+//     content.innerHTML =`<h1>My Contacts</h1>`
+// }
