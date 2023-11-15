9e65aa06612c195dfd077d0dbea236e964cc8c89
Rene Heller <terra7778@gmail.com>
2023-11-15 22:18:48 +0100

    button-logic finished

--- script.js
+++ script.js
@@ -30,66 +30,71 @@ fetch('quiz.json')
     })
     let currentQuestion = 0;
 
+
+//render question; initate further actions
 function showQuestion(number) {
     number= currentQuestion;
     let question = document.getElementById('question');
     let firstQuestion = cssQuestion[number].frage
     question.innerHTML =/*html*/`
-        ${firstQuestion}
+        <b>${firstQuestion}</b>
     `;
     answers();
     init()
 }
 
+
+//answer rendering
 function answers() {
     number = currentQuestion;
     let answers = document.getElementsByClassName('option');
-
     for (let i = 0; i < answers.length; i++) {
         answers[i].innerHTML = `${cssQuestion[number].antworten[i]}`
     }
 }
 
+
+//next question; restoring css properties
 function nextQuestion() {
     currentQuestion +=1;
     refresh();
     return showQuestion()
 }
 
+
+//reactivate default css for cards
 function refresh(){
     let cards = document.getElementsByClassName('option');
+    let button = document.getElementById("next");
     for(let i = 0; i<cards.length; i++){
-        cards[i].style.pointerEvents="all",
-        cards[i].style.backgroundColor="white",
-        cards[i].addEventListener("mouseenter",mEnter),        
-        cards[i].addEventListener("mouseout",mOut)
-
+        cards[i].parentNode.style.pointerEvents="all",
+        cards[i].parentNode.classList.remove('bg-success'),
+        cards[i].parentNode.classList.remove('bg-danger'),
+        cards[i].parentNode.addEventListener("mouseenter",mEnter),        
+        cards[i].parentNode.addEventListener("mouseout",mOut)
+        cards[i].style.color="black"
     }
+    button.disabled=true
 }
 
-
+// actual question and amount of questions
 function init(){
     let number = document.getElementById('question-number');
     let length = document.getElementById('question-length');
-
-    number.innerHTML=`${currentQuestion+1}`
-    length.innerHTML=`${cssQuestion.length}&nbsp`
+    number.innerHTML=`<b>${currentQuestion+1}</b>`
+    length.innerHTML=`<b>${cssQuestion.length}</b>&nbsp`
 }
 
+
+//result of clicked option
 function choice(id){
     let choice = document.getElementById(`${id}`);
-    let cards = document.getElementsByClassName('option');
     let number = currentQuestion;
-    if(choice.innerText == cssQuestion[number].richtige_antwort){
-        choice.style.backgroundColor="green"
-    }else{
-        choice.style.backgroundColor="red"
+    if(choice.innerText !== cssQuestion[number].richtige_antwort){
+        choice.parentNode.classList.add('bg-danger')
     };
-    for(let i = 0; i<cards.length; i++){
-        cards[i].style.pointerEvents="none",
-        cards[i].removeEventListener("mouseenter",mEnter),
-        cards[i].removeEventListener("mouseout",mOut)
-    }
+    choice.style.color="white";
+    changeCss();
 }
 
 function mEnter(){
@@ -98,4 +103,22 @@ function mEnter(){
 
 function mOut(){
     this.style.backgroundColor="white"
+};
+
+
+//disable further clickEvents after choosing an answer/css-changes
+function changeCss(){
+    let cards = document.getElementsByClassName('option');
+    let number = currentQuestion;
+    let button = document.getElementById("next");
+    for(let i = 0; i<cards.length; i++){
+        cards[i].parentNode.style.pointerEvents="none",
+        cards[i].parentNode.removeEventListener("mouseenter",mEnter),
+        cards[i].parentNode.removeEventListener("mouseout",mOut)
+        if(cards[i].innerText==cssQuestion[number].richtige_antwort){
+            cards[i].parentNode.classList.add('bg-success')
+            cards[i].style.color="white"
+        }
+    };
+    button.disabled=false;
 }
