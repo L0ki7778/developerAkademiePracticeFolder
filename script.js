faf55b37b7ea51b37ea3508fe1518cf08097aced
Rene Heller <terra7778@gmail.com>
2023-11-15 14:32:04 +0100

    implemented question ratio

--- script.js
+++ script.js
@@ -1,5 +1,62 @@
-// function render(){
-//    let content = document.getElementById("contacts");
-//     content.innerHTML = ``
-//     content.innerHTML =`<h1>My Contacts</h1>`
-// }
+let questions;
+let topicCss;
+let cssQuestion ;
+let topicHtml;
+let htmlQuestion;
+let topicJs;
+let jsQuestion;
+let topicPoke;
+let poliQuestion;
+
+fetch('quiz.json')
+    .then(response => {
+        if (!response.ok) {
+            alert("Fehler beim Laden der Fragen")
+        } else {
+            return response.json()
+        }
+    })
+    .then(data => {
+        questions = data;
+        topicCss = questions[0];
+        cssQuestion = topicCss.fragen;
+        topicHtml = questions[1];
+        htmlQuestion = topicHtml.fragen;
+        topicJs = questions[2];
+        jsQuestion = topicJs.fragen;
+        topicPoke = questions[3];
+        poliQuestion = topicPoke.fragen;
+        showQuestion()
+    })
+    let currentQuestion = 0;
+
+function showQuestion(number) {
+    number= currentQuestion;
+    let question = document.getElementById('question');
+    let firstQuestion = cssQuestion[number].frage
+    question.innerHTML =/*html*/`
+        ${firstQuestion}
+    `;
+    answers()
+}
+
+function answers() {
+    number = currentQuestion;
+    let answers = document.getElementsByClassName('option');
+
+    for (let i = 0; i < answers.length; i++) {
+        answers[i].innerHTML = `${cssQuestion[number].antworten[i]}`
+    }
+}
+
+function nextQuestion() {
+    currentQuestion +=1;
+    return showQuestion()
+}
+
+
+
+function init(){
+    let number = document.getElementById('question-number');
+    let length = document.getElementById('question-length');
+}
